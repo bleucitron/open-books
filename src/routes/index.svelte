def0c0f58399dd8db217647d57f073b50a2563dd
Romain Crestey <rcrestey@gmail.com>
2020-03-20 10:32:01 +0100

    style(index): remove useless style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -81,17 +81,12 @@
 </script>
 
 <style>
-  h1,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
   h1 {
+    margin: 0 auto;
     font-size: 2.8em;
     text-transform: uppercase;
     font-weight: 700;
-    margin: 0 0 0.5em 0;
+    text-align: center;
   }
 
   @media (min-width: 480px) {
