fc0d9e6f295a1e9b6538faf94aa43f1edc11d448
Romain Crestey <rcrestey@gmail.com>
2020-03-29 12:32:14 +0200

    docs: add comment for getBudgets

--- src/api/budget.js
+++ src/api/budget.js
@@ -4,6 +4,7 @@ import { makeBudgetEndpoint } from '../utils/budget';
 const baseURL = 'https://data.economie.gouv.fr';
 
 export function getBudgets(siren, code, year = 2018) {
+  // the code argument might not necessary since siren identitifies the city, supposably
   const endpoint = makeBudgetEndpoint(siren, code, year);
 
   return get(endpoint, {
