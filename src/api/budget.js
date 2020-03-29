600c2b83d9c69a54318b79f0087eb65ed293809f
Romain Crestey <rcrestey@gmail.com>
2020-03-29 14:36:29 +0200

    fix(api): restore simple budget endpoint

--- src/api/budget.js
+++ src/api/budget.js
@@ -1,11 +1,11 @@
 import { get } from '../utils/verbs';
-import { makeBudgetEndpoint } from '../utils/budget';
+import { makeBudgetCroiseEndpoint } from '../utils/budget';
 
 const baseURL = 'https://data.economie.gouv.fr';
 
 export function getBudgets(siren, code, year = 2018) {
   // the code argument might not necessary since siren identitifies the city, supposably
-  const endpoint = makeBudgetEndpoint(siren, code, year);
+  const endpoint = makeBudgetCroiseEndpoint(siren, code, year);
 
   return get(endpoint, {
     baseURL,
--- src/utils/budget.js
+++ src/utils/budget.js
@@ -10,9 +10,26 @@ const byYear = {
   '2017': '-',
 };
 
-export function makeBudgetEndpoint(siren, code, year) {
-  const base = '/api/records/1.0/search';
+const base = '/api/records/1.0/search';
 
+export function makeBudgetSimpleEndpoint(siren, code, year) {
+  const dataset = `dataset=balances-comptables-des-communes-en-${year}`;
+
+  const c = code.slice(2);
+  const sirenParam = `siren:${siren}`;
+  const inseeParam = `insee:${c}`;
+
+  const params = [sirenParam, inseeParam].join('&');
+  const query = `q=${params}`;
+
+  const rows = `rows=${nbResults}`;
+
+  const allParams = [dataset, query, rows].join('&');
+
+  return `${base}?${allParams}`;
+}
+
+export function makeBudgetCroiseEndpoint(siren, code, year) {
   const dataset = `balances-comptables-des-collectivites-et-des-etablissements-publics-locaux-avec${byYear[year]}`;
 
   const c = code.slice(2);
