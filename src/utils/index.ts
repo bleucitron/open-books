e788955743110614d18c2983eef2aca87a92a94f
Romain Crestey <rcrestey@gmail.com>
2020-09-08 12:08:09 +0200

    fix(Years): use casting to force number[] type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -64,10 +64,6 @@ export function normalizeText(text: string): string {
   return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
 
-export function isNumber(v): v is number {
-  return typeof v === 'number';
-}
-
 export function formatValue(value: number): string {
   return new Intl.NumberFormat('fr', {
     style: 'currency',
