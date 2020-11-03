8d05fcdfe4531d2c5bd105a90356363d44171a28
Romain Crestey <rcrestey@gmail.com>
2020-11-03 18:08:10 +0100

    feat(utils): add utils for model fetching and parsing

--- src/api/budget.ts
+++ src/api/budget.ts
@@ -1,14 +1,56 @@
 import { get } from './utils/verbs';
-import { makeBudgetCroiseEndpoint } from './utils/budget';
+import { makeBudgetCroiseEndpoint, makeModelEndpoint } from './utils/budget';
 
 import type { Record, BudgetParams } from '../interfaces';
 
-const baseURL = 'https://data.economie.gouv.fr';
+const recordsUrl = 'https://data.economie.gouv.fr';
 
 export function getRecords(params: BudgetParams): Promise<Record[]> {
   const endpoint = makeBudgetCroiseEndpoint(params);
 
   return get(endpoint, {
-    baseURL,
+    baseURL: recordsUrl,
   }).then(({ records }) => records.map(record => record.fields));
+}
+
+const modelUrl =
+  'https://raw.githubusercontent.com/iOiurson/plans-de-compte/main/';
+
+function extractFunctions(e: Element) {
+  if (e.tagName !== 'RefFonc' && e.tagName !== 'RefFonctionnelles')
+    throw `${e.tagName} Not a <RefFonc> or a <RefFonctionnelle>`;
+
+  const data = [...e.children].map(element => {
+    const code = element.getAttribute('Code');
+    const label = element.getAttribute('Libelle');
+    const short = element.getAttribute('Lib_court');
+
+    return [
+      code,
+      { code, label, short, subFunctions: extractFunctions(element) },
+    ];
+  });
+
+  return Object.fromEntries(data);
+}
+
+function makeFunctionTree(txt: string) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(txt, 'application/xml');
+
+  const refFonc = doc.querySelector('RefFonctionnelles');
+
+  if (!refFonc) throw 'No <RefFonctionnelles> found';
+
+  return extractFunctions(refFonc);
+}
+
+export function getModel(year: number, code: string, population: number) {
+  const endpoint = makeModelEndpoint(year, code, population);
+
+  console.log('FILE NAME', endpoint);
+
+  return get(endpoint, {
+    baseURL: modelUrl,
+  }).then(makeFunctionTree);
 }
