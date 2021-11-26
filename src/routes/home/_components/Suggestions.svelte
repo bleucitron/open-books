8cc440e0335e6cfd0e90534d42315f66110d34a9
Matthieu THOLOT-DECHENE <mtholot19@gmail.com>
2021-11-26 16:25:25 +0100

    feat: update après review

--- src/routes/home/_components/Suggestions.svelte
+++ src/routes/home/_components/Suggestions.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-
   import type { City } from '@interfaces';
   import Spinner from '../../_components/Spinner.svelte';
 
@@ -8,22 +7,13 @@
   export let select: (c: City) => void;
   export let city: City = undefined;
 
-  let current = 0;
-  let key;
-  let numberCity: number;
-  function mousseInGestion(id): void {
-    current = id + 1;
-  }
-  function mousseOutGestion(): void {
-    current = 0;
-  }
-  function keyboardGestion(event): void {
-    numberCity = suggestions.length;
-    key = event.key;
-    switch (key) {
+  let current: number = undefined;
+
+  function keyboardGestion(e: KeyboardEvent): number {
+    switch (e.key) {
       case 'ArrowUp': {
-        if (current === 1 || current === 0) {
-          current = numberCity;
+        if (current === 0 || current === undefined) {
+          current = number;
         } else {
           current = current - 1;
         }
@@ -31,18 +21,25 @@
       }
 
       case 'ArrowDown': {
-        current = current === numberCity ? 1 : current + 1;
+        if (current === number || current === undefined) {
+          current = 0;
+        } else {
+          current = current + 1;
+        }
         break;
       }
 
       case 'Enter': {
-        const url = document.getElementById(current.toString()).href;
-        select(suggestions[current - 1]);
-        goto(url);
+        const suggest = suggestions[current];
+        select(suggest);
+        goto(`/budgets?name=${suggest.nom}&insee=${suggest.code}`);
         break;
       }
     }
+    return current;
   }
+
+  $: number = suggestions.length - 1;
 </script>
 
 <svelte:window on:keyup={keyboardGestion} />
@@ -50,13 +47,11 @@
   {#each suggestions as suggestion, index (index)}
     <li class="Suggestion" on:click={() => select(suggestion)}>
       <a
-        on:mouseenter={mousseInGestion(index)}
-        on:mouseleave={mousseOutGestion()}
+        on:mouseenter={() => (current = index)}
+        on:mouseleave={() => (current = 0)}
         href={`/budgets?name=${suggestion.nom}&insee=${suggestion.code}`}
         rel="prefetch"
-        id={index + 1}
-        class:keyboard={current === index + 1}
-        class:keyboard-black={(current = !0)}
+        class:active={current === index}
       >
         <div class="infos">
           <div class="name">{suggestion.nom}</div>
@@ -86,26 +81,11 @@
     padding: 0.5rem 1rem;
   }
 
-  .keyboard {
+  .active {
     background: coral !important;
     color: white;
     cursor: pointer;
   }
-  .keyboard-black {
-    background: #555;
-    display: flex;
-    padding: 0.5rem 1rem;
-  }
-  .keyboard-enter {
-    background: coral !important;
-    color: white;
-    cursor: pointer;
-  }
-
-  .no-hover {
-    pointer-events: none;
-  }
-
   .infos {
     display: flex;
   }
