060fd97fb9c28110d01cfcbd2b9fdda1790dc40d
Germain Grumel <germaingrumel@outlook.fr>
2022-03-04 14:12:50 +0100

    feat: wip

--- src/routes/compare/_components/Search.svelte
+++ src/routes/compare/_components/Search.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
+
+  import { getCities } from '@api';
+  import type { City } from '@interfaces';
+
   let value = '';
-  const handleInput = (e: Event): void => {
+  let cities: City[] = [];
+  const chosenCity: Array<string> = [];
+
+  $: console.log('pouet', chosenCity.length);
+  $: console.log('cities', cities);
+
+  $: if (chosenCity.length === 2) {
+    goto(
+      `compare-result?sirets=21330063500017,21690123100011&year=2019&cities=${chosenCity[0]},${chosenCity[1]}`,
+    );
+  }
+
+  const handleCityClick = (city: string): void => {
+    chosenCity.push(city);
+  };
+  const handleInput = async (e: Event): Promise<void> => {
     const target = e.target as HTMLInputElement;
     value = target.value;
+    cities = await getCities(value);
   };
 </script>
 
 <input type="text" bind:value on:input={handleInput} />
 
-<div>{value}</div>
+{#if cities.length > 0}
+  {#each cities as city}
+    <div on:click={() => handleCityClick(city.nom)}>{city.nom}</div>
+  {/each}
+{/if}
