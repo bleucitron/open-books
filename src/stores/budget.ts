0594824a0cde22b629c0a8ef2afb290ef46792c2
Romain Crestey <rcrestey@gmail.com>
2022-04-09 20:57:02 +0200

    refactor: move all budget-related stores in same file

--- src/stores/budget.ts
+++ src/stores/budget.ts
@@ -1,5 +1,32 @@
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 
-import type { Budget } from '@interfaces';
+import type { Budget, Type, FonctionTreeValue } from '@interfaces';
+import { stepsFromString, fonctionFromTree } from '@utils';
 
-export default writable<Budget>();
+const budget = writable<Budget>();
+
+export default budget;
+
+export const type = writable<Type>();
+
+export const code = writable<string>();
+
+export const tree = derived([budget, code], ([$budget, $code]) => {
+  if (!$budget) return null;
+  if (!$code) return $budget;
+
+  const steps = stepsFromString($code);
+
+  return steps.reduce<Budget | FonctionTreeValue>(
+    (acc, cur) => acc.tree[cur],
+    $budget,
+  );
+});
+
+export const fonction = derived([code, budget], ([$code, $budget]) => {
+  if (!$code || !$budget) {
+    return undefined;
+  }
+
+  return fonctionFromTree($code, $budget.tree);
+});
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,2 @@
 export { default as city } from './city';
-export { default as budget } from './budget';
-export { default as code } from './code';
-export { default as tree } from './tree';
-export { default as fonction } from './fonction';
+export { default as budget, type, code, tree, fonction } from './budget';
