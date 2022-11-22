049f475365845fdf87e3a260cc496fa4f06bad56
Romain Crestey <rcrestey@gmail.com>
2022-11-22 14:04:49 +0100

    refactor: redirect to 404 for unknown insee

--- src/routes/commune/+layout.ts
+++ src/routes/commune/+layout.ts
@@ -1,10 +1,13 @@
+import { error } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 import { getCity } from '@api/geo';
 
 export const load: LayoutLoad = async ({ params, fetch }) => {
   const insee = params.insee ?? '';
 
-  const city = await getCity(insee, fetch);
+  const city = await getCity(insee, fetch).catch(() => {
+    throw error(404, `Le code INSEE ${insee} n'existe pas`);
+  });
 
   return {
     city,
