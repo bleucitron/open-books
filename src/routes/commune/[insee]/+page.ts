487960c79875e10646689f4dcd7191f899d645a3
Romain Crestey <rcrestey@gmail.com>
2022-11-22 12:44:51 +0100

    fix: avoid redirect loop

--- src/routes/commune/[insee]/+page.ts
+++ src/routes/commune/[insee]/+page.ts
@@ -2,26 +2,27 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 import { fillBudget } from './cache';
 
-export const load: LayoutLoad = async ({ params, url, parent, fetch }) => {
-  const { insee } = params;
+const defaultYear = new Date().getFullYear() - 2;
+
+export const load: LayoutLoad = async ({ url, parent, fetch }) => {
   const { searchParams } = url;
   const siret = searchParams.get('siret');
-  const year = parseInt(searchParams.get('year') ?? '');
+  const year = searchParams.get('year');
 
-  const data = await parent();
-
-  const { city } = data;
+  const { city, siret: mainSiret } = await parent();
 
   if (!siret || !year) {
-    const s = siret ?? data.siret;
-    const defaultYear = new Date().getFullYear() - 2;
+    const s = siret ?? mainSiret;
+    const y = year ?? defaultYear.toString();
 
-    const redirectUrl = `/commune/${insee}?year=${defaultYear}&siret=${s}`;
+    const redirectUrl = new URL(url);
+    redirectUrl.searchParams.set('siret', s);
+    redirectUrl.searchParams.set('year', y);
 
-    throw redirect(302, redirectUrl);
+    throw redirect(302, redirectUrl.toString());
   }
 
-  const budget = await fillBudget(siret, year, city, fetch);
+  const budget = await fillBudget(siret, parseInt(year), city, fetch);
 
   return {
     budget,
